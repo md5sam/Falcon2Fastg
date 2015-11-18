79deff60ac43dad3f58c79dd4e7a09c260a84e3e
samarth <samarth@belseels.lifl.fr>
2015-11-18 11:28:27 +0100

    updated Falcon2Fastg.py with comments and style improvements

--- Falcon2Fastg.py
+++ Falcon2Fastg.py
@@ -7,12 +7,13 @@ from Bio.SeqIO import FastaIO
 list_of_tuples = []
 all_nodes_list = []
 all_source_nodes = []
-direction = defaultdict(list)
+follows = defaultdict(list)
 
 
 sys.stdout = open('output.fastg', 'w')
 
 
+# converts multiline preads4falcon.fasta into a single line fasta
 def convert_multiline_to_single_line_FASTA () :
     sequences = [] 
     input_handle = open("preads4falcon.fasta", "rU")
@@ -26,6 +27,7 @@ def convert_multiline_to_single_line_FASTA () :
     output_handle.close()
 
 
+# creates a tuple for every pair in sg_edges_list
 def create_read_pair_tuples () :
     with open("sg_edges_list") as sg_entries:
         read_pairs_TMI = csv.reader(sg_entries, delimiter=' ')
@@ -34,25 +36,42 @@ def create_read_pair_tuples () :
             list_of_tuples.append(read_pair_tuple)
     list_of_tuples.sort()	    
 
-
-def make_direction_dict () :
+# collapses multiple tuples into a dictionary
+# key is the first entry in a tuple; each key represents "Source" node
+# value(s) is(are) the second entry(entries) in tuple(s); are the "Sink" nodes
+def make_follows_dict () :
     for pair in list_of_tuples :
-	if pair[0][-1] == "E" and pair[1][-1] == "E" :
-	    direction[pair[0][:-2]].append(pair[1][:-2])
-            
-	elif pair[0][-1] == "E" and pair[1][-1] == "B" :
-	    direction[pair[0][:-2]].append(pair[1][:-2]+"'")
-                        
-	elif pair[0][-1] == "B" and pair[1][-1] == "E" :
-            direction[pair[0][:-2]+"'"].append(pair[1][:-2])
+	start_node_location = pair[0][-1] 
+        end_node_location = pair[1][-1]
+        
+        source_node = pair[0][:-2]
+        sink_node = pair[1][:-2]
 
-	elif pair[0][-1] == "B" and pair[1][-1] == "B" :
-            direction[pair[0][:-2]+"'"].append(pair[1][:-2]+"'")
+        if start_node_location == "E" and end_node_location == "E" :
+	    follows[source_node].append(sink_node)
+            
+	elif start_node_location == "E" and end_node_location == "B" :
+	    follows[source_node].append(sink_node+"'")
+                        
+	elif start_node_location == "B" and end_node_location == "E" :
+            follows[source_node+"'"].append(sink_node)
+
+	elif start_node_location == "B" and end_node_location == "B" :
+            follows[source_node+"'"].append(sink_node+"'")
 	    
         else : 
 	    print "ERROR : the tuples have not been parsed correctly"    
 
 
+# used by make_fastg() to provide padding for the header
+def headerify (node) :
+    if node[-1] == "'" :
+        return str("NODE_"+node[:-1]+"_length_"+"500"+"_cov_50'")
+    else :
+        return str("NODE_"+node+"_length_"+"500"+"_cov_50")
+
+
+# used by make_fastg() to extract sequence given record name in FASTA file
 def corresponding_FASTA(record_name): 
     if record_name[-1] == "'" :
         corrected_name = record_name[:-1]
@@ -78,15 +97,10 @@ def corresponding_FASTA(record_name):
                 return 
 
 
-def headerify (node) :
-    if node[-1] == "'" :
-	return str("NODE_"+node[:-1]+"_length_"+"500"+"_cov_50'") 
-    else : 
-	return str("NODE_"+node+"_length_"+"500"+"_cov_50")    
-
-
+# calls headerify() and corresponding_fasta() on 'follows' dictionary
+# makes the first part of FASTG file which has nodes with overlaps 
 def make_fastg() :
-    for source, sinks in direction.items() : 
+    for source, sinks in follows.items() : 
         all_source_nodes.append(source)	    
 	header = []
 	header.append(">"+headerify(source)+":")
@@ -98,6 +112,7 @@ def make_fastg() :
 	corresponding_FASTA(source)
 
 
+# makes the second part of FASTG file which has fwd nodes without overlaps
 def print_non_sources() :
     fp_non = open("formatted_preads4falcon.fasta")
     NON_flag = "non_source"
@@ -116,14 +131,15 @@ def print_non_sources() :
     fp_non.close()     
 
 
+# makes the third part of FASTG file which has revcomp nodes without overlaps
 def print_non_sources_complement() :
     fp_non = open("formatted_preads4falcon.fasta")
     NON_flag = "non_source" 
     for line in fp_non :
 	if line[0] == ">":		
 	    if str (line[1:-1]+"'") not in all_source_nodes :
-	        sys.stdout.write(">NODE_"+line[1:-1]+"_length_"+"500"+"_cov_50';")								                
-		print
+		sys.stdout.write(">NODE_"+line[1:-1]+"_length_"+"500"+"_cov_50';")
+                print
 		NON_flag = "non_source"
 	    else :
 		NON_flag = "source"
@@ -137,9 +153,10 @@ def print_non_sources_complement() :
     fp_non.close() 
 
 
+
 convert_multiline_to_single_line_FASTA ()
 create_read_pair_tuples()
-make_direction_dict()
+make_follows_dict()
 make_fastg()
 print_non_sources()
 print_non_sources_complement()
